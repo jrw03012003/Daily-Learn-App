5e00075431baea577ffb3c44c30ad5e210d816dc
JRW <jrw.jackwhite@gmail.com>
2024-10-21 15:26:08 +0100

    Initial Commit

---
+++ test 2 app/test_2_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_2_appApp.swift
+//  test 2 app
+//
+//  Created by Jack white on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct test_2_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test 2 appTests/test_2_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_2_appTests.swift
+//  test 2 appTests
+//
+//  Created by Jack white on 21/10/2024.
+//
+
+import Testing
+@testable import test_2_app
+
+struct test_2_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
